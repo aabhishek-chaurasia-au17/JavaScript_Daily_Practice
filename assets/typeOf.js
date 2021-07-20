a808ecf926103f8abe2af90698b9669e400c7f7b
aabhishek-chaurasia-au17 <abhishekc838@gmail.com>
2021-07-20 06:55:59 +0530

    typeOf

---
+++ assets/typeOf.js
@@ -0,0 +1,20 @@
+// #### Q1
+// ### Show the usage of `typeof` operator on different types of values
+
+// - The `typeof` operator returns a string indicating the type of the operand
+
+console.log(typeof(50));
+console.log(typeof("text"));
+console.log(typeof(true));
+console.log(typeof(undefined));
+console.log(typeof(function(){}));
+console.log(typeof({}));
+console.log(typeof(10n));
+console.log(typeof(Symbol()));
+console.log(typeof([1, 2]));
+console.log(typeof(NaN));
+console.log(typeof(undeclaredVar));
+console.log(typeof(Infinity));
+console.log(typeof(null));
+console.log(typeof(/regex/));
+
