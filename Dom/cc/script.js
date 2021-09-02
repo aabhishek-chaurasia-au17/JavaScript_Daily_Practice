27c4dab7efccc3cf0ab66a2c7b3a3fe0c68282f1
Abhishek <abhishekc838@gmail.com>
2021-09-02 06:04:39 +0530

    DOM

---
+++ Dom/cc/script.js
@@ -0,0 +1,18 @@
+
+let push = document.getElementById('Push');
+let pop = document.getElementById('Pop');
+let res = document.getElementById('result');
+var arr = []
+
+
+push.addEventListener('click', function () {
+    let input = document.getElementById('Input').value;
+    arr.push(input)
+    res.innerText = arr
+    console.log(arr);
+})
+
+pop.addEventListener("click", function(){
+    arr.pop()
+    res.innerText=arr
+})
