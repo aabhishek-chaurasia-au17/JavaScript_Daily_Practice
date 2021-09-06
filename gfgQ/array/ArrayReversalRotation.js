8498a5c0d537f8b236a1b47152d768f9d9ee02e8
Abhishek <abhishekc838@gmail.com>
2021-09-06 08:03:10 +0530

    practice

---
+++ gfgQ/array/ArrayReversalRotation.js
@@ -0,0 +1,20 @@
+// Program for Reversal algorithm for array rotation
+
+// Example :
+
+// Input :  arr[] = [1, 2, 3, 4, 5, 6, 7]
+//          d = 2
+// Output : arr[] = [3, 4, 5, 6, 7, 1, 2] 
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+const getreversalarray = (arr,d) =>{
+    for(let i = 0; i < d; i++){
+        arr.shift(arr.unshift())
+    }
+    
+}
+
+let d = 2;
+
+console.log( getreversalarray(arr,d));
